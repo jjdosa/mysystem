9b2f29c0832fde7711b98dbf90d8b63fb4f22779
JJ Kim <jj@haedosa.xyz>
2023-10-24 20:44:36 +0900

    add builder7 to substituters

--- nixosConfigurations/features/substituters/hds0.nix
+++ nixosConfigurations/features/substituters/hds0.nix
@@ -1,6 +1,7 @@
 {
   nix.settings = {
     substituters = [
+      "http://10.10.100.7"
       "http://10.10.100.8"
       "http://10.10.100.3"
       # "http://10.10.100.4"
@@ -9,7 +10,7 @@
     ];
     trusted-public-keys = [
       "10.10.100.8:03dsPxeLnFnAdR7hhUqPH6rwNow0dHt49KWqzS3AjC8="
-      "10.10.100.7:JnrjqEdbhUv0ErVWbUoVRgKQKlNu89r5BhyXz8kJdDE="
+      "10.10.100.7:6FTRW2lRX1jjSXflQPvAnBzvkaCnchMO4y4wj7y8PFI="
       "10.10.100.3:ICrJJg0EV8V5n90xghprYM7hEZg+dJ5T06gyaHqZtKU="
       "10.10.100.4:g2y9eiBfz+zWX6PGbXSxiRcJcW6+7RFZh0TXwF8cmcc="
       "10.10.100.5:+3i3teuBVBQXR47k9M0zLVmdzirKSGm9+9awX2jp+u0="
